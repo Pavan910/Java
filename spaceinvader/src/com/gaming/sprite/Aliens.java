069e0d86d1ca7f74bceca594ba150565465dc468
Pavan910 <87536471+Pavan910@users.noreply.github.com>
2023-04-18 23:25:10 +0530

    Add files via upload

---
+++ spaceinvader/src/com/gaming/sprite/Aliens.java
@@ -0,0 +1,68 @@
+package com.gaming.sprite;
+
+import javax.swing.ImageIcon;
+
+public class Aliens extends Sprite {
+
+    private Bomb bomb;
+
+    public Aliens(int x, int y) {
+
+        initAlien(x, y);
+    }
+
+    private void initAlien(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+
+        bomb = new Bomb(x, y);
+
+        var alienImg = "src/alien.png";
+        var ii = new ImageIcon(alienImg);
+
+        setImage(ii.getImage());
+    }
+
+    public void act(int direction) {
+
+        this.x += direction;
+    }
+
+    public Bomb getBomb() {
+
+        return bomb;
+    }
+
+    public class Bomb extends Sprite {
+
+        private boolean destroyed;
+
+        public Bomb(int x, int y) {
+
+            initBomb(x, y);
+        }
+
+        private void initBomb(int x, int y) {
+
+            setDestroyed(true);
+
+            this.x = x;
+            this.y = y;
+
+            var bombImg = "src/images/bomb.png";
+            var ii = new ImageIcon(bombImg);
+            setImage(ii.getImage());
+        }
+
+        public void setDestroyed(boolean destroyed) {
+
+            this.destroyed = destroyed;
+        }
+
+        public boolean isDestroyed() {
+
+            return destroyed;
+        }
+    }
+}
